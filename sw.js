853c6544d639bc13c01c76fdec381456f11a1849
oldejollyroger <rsaladie31@gmail.com>
2025-06-15 22:54:31 +0200

    Remove unused PWA files, update script version for cache refresh, and streamline region handling

--- sw.js
+++
@@ -1,6 +0,0 @@
-// sw.js
-
-// This is the simplest possible service worker to make the app installable.
-self.addEventListener('fetch', (event) => {
-  // We are not adding any offline logic yet, just fulfilling the requirement.
-});
